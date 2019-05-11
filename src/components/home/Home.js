2bf15ff194c9289d75aeb4ce82403360389e5cdb
jhtong <me@joeltong.org>
2019-05-11 11:44:56 +0800

    added grid to component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,41 +4,81 @@ import PropTypes from "prop-types";
 import { Stage, Layer, Rect, Text } from "react-konva";
 import Konva from "konva";
 
-class ColoredRect extends React.Component {
-  state = {
-    color: "green"
+class CustomRect extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleOver = this.handleOver.bind(this);
+  }
+
+  handleOver = () => {
+    const { x, y } = this.props;
+    console.log(x, y);
   };
-  handleClick = () => {
-    this.setState({
-      color: Konva.Util.getRandomColor()
-    });
-  };
+
+  // handleClick = () => {
+  //   this.setState({
+  //     color: Konva.Util.getRandomColor()
+  //   });
+  // };
   render() {
+    const { x, y, xPos, yPos, width, height } = this.props;
     return (
       <Rect
-        x={20}
-        y={20}
-        width={50}
-        height={50}
-        fill={this.state.color}
-        shadowBlur={5}
-        onClick={this.handleClick}
+        x={xPos}
+        y={yPos}
+        width={width}
+        height={height}
+        stroke="#fff"
+        strokeWidth={1}
+        onMouseEnter={this.handleOver}
       />
     );
   }
 }
 
+const makeGrid = ({ width = 50, height = 50, numX = 0, numY = 0 }) => {
+  const rects = [];
+  const rectWidth = width / numX;
+  const rectHeight = height / numY;
+  for (let y = 0; y < numY; y++) {
+    for (let x = 0; x < numX; x++) {
+      rects.push(
+        <CustomRect
+          x={x}
+          y={y}
+          xPos={x * rectWidth}
+          yPos={y * rectHeight}
+          width={rectWidth}
+          height={rectHeight}
+        />
+      );
+    }
+  }
+  return rects;
+};
+
 const Home = ({ props }) => {
+  const rects = makeGrid({
+    width: 933,
+    height: window.innerHeight,
+    numX: 100,
+    numY: 100
+  });
+
   return (
     <div
       style={{
-        background: "#ccc"
+        width: 933,
+        maxWidth: 933,
+        background: "#eee"
       }}
     >
-      <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Stage width={933} height={window.innerHeight}>
         <Layer>
-          <Text text="Try click on rect" />
-          <ColoredRect />
+          {/* <Text text="Try click on rect" /> */}
+          {/* <ColoredRect /> */}
+          {rects.map(r => r)}
         </Layer>
       </Stage>
     </div>
